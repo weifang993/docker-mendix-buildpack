b96a83d0adadf50169f87b05f9e97aadef457eca
weifang993 <wfang.ca@gmail.com>
2019-05-09 09:58:32 -0400

    removed ssotest

--- SSOTest/javasource/saml20/implementation/common/URLUtils.java
+++
@@ -1,8 +0,0 @@
-package saml20.implementation.common;
-
-public final class URLUtils {
-
-    public static String ensureEndsWithSlash(String text) {
-        return text.endsWith("/") ? text : text + "/";
-    }
-}
